2cf2bc2e4f0b998a71b9119f66dfa5b691769a05
Sehran Agayev <sehran.a@bestcomp.net>
2021-09-25 23:25:05 +0400

    first commit

---
+++ PPMToolBackEnd/src/main/java/ip/agileintellligense/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package ip.agileintellligense.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
